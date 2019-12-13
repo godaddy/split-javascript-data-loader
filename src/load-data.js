865003ce1ad21883bb55c335b73edc1373b61c5f
Rohan Murthy <rohanmur@hotmail.com>
2019-12-13 13:33:59 -0800

    remove consts

--- src/load-data.js
+++ src/load-data.js
@@ -1,27 +1,27 @@
 'use strict'
 
-import { SEGMENT_SUB_KEY, SPLIT_SUB_KEY, TILL, USING_SEGMENTS } from './localStorage-keys'
+const TILL_KEY = 'SPLITIO.splits.till'
 
 export default function loadDataIntoLocalStorage ({ serializedData = {} }) {
   const { segmentsData, since, splitsData, userId, usingSegmentsCount } = serializedData
   const windowLocalStorage = window.localStorage
 
   // Do not load data if current localStorage data is more recent
-  if (since <= windowLocalStorage.getItem(TILL)) {
+  if (since <= windowLocalStorage.getItem(TILL_KEY)) {
     return
   }
   windowLocalStorage.clear()
-  windowLocalStorage.setItem(TILL, since)
+  windowLocalStorage.setItem(TILL_KEY, since)
 
   // splitsData in an object where the property is the split name and the pertaining value is a stringified json of its data
   Object.keys(splitsData).forEach(splitName => {
-    windowLocalStorage.setItem(`${SPLIT_SUB_KEY}.${splitName}`, splitsData[splitName])
+    windowLocalStorage.setItem(`SPLITIO.split.${splitName}`, splitsData[splitName])
   })
 
-  windowLocalStorage.setItem(USING_SEGMENTS, usingSegmentsCount)
+  windowLocalStorage.setItem('SPLITIO.splits.usingSegments', usingSegmentsCount)
   // segmentsData in an object where the property is the segment name and the pertaining value is an array of userIds
   Object.keys(segmentsData).forEach(segmentName => {
-    const key = `${userId}.${SEGMENT_SUB_KEY}.${segmentName}`
+    const key = `${userId}.SPLITIO.segment.${segmentName}`
     if (segmentsData[segmentName].includes(userId) && windowLocalStorage.getItem(key) !== '1') {
       windowLocalStorage.setItem(key, '1')
     }
--- src/load-data.test.js
+++ src/load-data.test.js
@@ -4,8 +4,8 @@ import { expect } from 'chai'
 import { JSDOM } from 'jsdom'
 
 import loadDataIntoLocalStorage from './load-data'
-import { TILL, USING_SEGMENTS } from './localStorage-keys'
 
+const TILL_KEY = 'SPLITIO.splits.till'
 const SMALLER_SINCE = 0
 const LARGER_SINCE = 1
 
@@ -18,7 +18,7 @@ describe('lib.load-data.loadDataIntoLocalStorage', () => {
   it('should not affect localStorage if its data is more recent', () => {
     const nonClearedItemKey = 'should_not_be_cleared'
     window.localStorage.setItem(nonClearedItemKey, true)
-    window.localStorage.setItem(TILL, LARGER_SINCE)
+    window.localStorage.setItem(TILL_KEY, LARGER_SINCE)
 
     loadDataIntoLocalStorage({ serializedData: { since: SMALLER_SINCE } })
 
@@ -28,13 +28,13 @@ describe('lib.load-data.loadDataIntoLocalStorage', () => {
   it('should clear localStorage and change till to more recent value with larger since value', () => {
     const clearedItemKey = 'should_be_cleared'
     window.localStorage.setItem(clearedItemKey, true)
-    window.localStorage.setItem(TILL, SMALLER_SINCE)
+    window.localStorage.setItem(TILL_KEY, SMALLER_SINCE)
 
     const serializedData = { segmentsData: {}, since: LARGER_SINCE, splitsData: {}, userId: '', usingSegmentsCount: 0 }
     loadDataIntoLocalStorage({ serializedData })
 
     expect(window.localStorage.getItem(clearedItemKey)).to.equal(null)
-    expect(window.localStorage.getItem(TILL)).to.equal(LARGER_SINCE.toString())
+    expect(window.localStorage.getItem(TILL_KEY)).to.equal(LARGER_SINCE.toString())
   })
 
   it('should load serialized split data into localStorage properly', () => {
@@ -44,7 +44,7 @@ describe('lib.load-data.loadDataIntoLocalStorage', () => {
       experiment_1: serializedExperimentOne,
       experiment_2: serializedExperimentTwo
     }
-    window.localStorage.setItem(TILL, SMALLER_SINCE)
+    window.localStorage.setItem(TILL_KEY, SMALLER_SINCE)
 
     const serializedData = { segmentsData: {}, since: LARGER_SINCE, splitsData, userId: '', usingSegmentsCount: 0 }
     loadDataIntoLocalStorage({ serializedData })
@@ -62,12 +62,12 @@ describe('lib.load-data.loadDataIntoLocalStorage', () => {
       segment_1: [userId, 'visitor_guid_2'],
       segment_2: [userId, 'visitor_guid_3']
     }
-    window.localStorage.setItem(TILL, SMALLER_SINCE)
+    window.localStorage.setItem(TILL_KEY, SMALLER_SINCE)
 
     const serializedData = { segmentsData, since: LARGER_SINCE, splitsData: {}, userId, usingSegmentsCount }
     loadDataIntoLocalStorage({ serializedData })
 
-    expect(window.localStorage.getItem(USING_SEGMENTS)).to.equal(usingSegmentsCount.toString())
+    expect(window.localStorage.getItem('SPLITIO.splits.usingSegments')).to.equal(usingSegmentsCount.toString())
     expect(window.localStorage.getItem('visitor_guid_1.SPLITIO.segment.segment_1')).to.equal('1')
     expect(window.localStorage.getItem('visitor_guid_1.SPLITIO.segment.segment_2')).to.equal('1')
   })
