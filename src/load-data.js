d3af8375ecbaba51c9ba827563f80dacbc534c73
Rohan Murthy <rohanmur@hotmail.com>
2019-12-17 10:20:42 -0800

    feat(split-data-loader): implement load-data function (#5)

--- src/load-data.js
+++ src/load-data.js
@@ -1,5 +1,33 @@
 'use strict'
 
-export default function loadDataIntoLocalStorage ({ serializedData = {} }) {
-  throw new Error('Not implemented')
+const TILL_KEY = 'SPLITIO.splits.till'
+
+export default function loadDataIntoLocalStorage ({ serializedData = {}, userId = '' }, windowLocalStorage = {}) {
+  const { segmentsData, since, splitsData, usingSegmentsCount } = serializedData
+
+  // Do not load data if current localStorage data is more recent
+  if (since <= windowLocalStorage.getItem(TILL_KEY)) {
+    return
+  }
+  // Split.IO recommends cleaning up the localStorage data
+  Object.keys(windowLocalStorage).forEach(key => {
+    if (key.includes('SPLITIO')) {
+      windowLocalStorage.removeItem(key)
+    }
+  })
+  windowLocalStorage.setItem(TILL_KEY, since)
+
+  // splitsData in an object where the property is the split name and the pertaining value is a stringified json of its data
+  Object.keys(splitsData).forEach(splitName => {
+    windowLocalStorage.setItem(`SPLITIO.split.${splitName}`, splitsData[splitName])
+  })
+
+  windowLocalStorage.setItem('SPLITIO.splits.usingSegments', usingSegmentsCount)
+  // segmentsData in an object where the property is the segment name and the pertaining value is an array of userIds
+  Object.keys(segmentsData).forEach(segmentName => {
+    const key = `${userId}.SPLITIO.segment.${segmentName}`
+    if (segmentsData[segmentName].includes(userId) && windowLocalStorage.getItem(key) !== '1') {
+      windowLocalStorage.setItem(key, '1')
+    }
+  })
 }
--- src/load-data.test.js
+++ src/load-data.test.js
@@ -1,16 +1,74 @@
 'use strict'
 
 import { expect } from 'chai'
+import sinon from 'sinon'
 
 import loadDataIntoLocalStorage from './load-data'
 
+const SMALLER_SINCE = 0
+const LARGER_SINCE = 1
+
 describe('lib.load-data.loadDataIntoLocalStorage', () => {
-  it('should throw a not implemented error', () => {
-    try {
-      loadDataIntoLocalStorage({})
-    } catch (e) {
-      return expect(e.message).equals('Not implemented')
+  let localStorageOverride
+  beforeEach(() => {
+    localStorageOverride = {
+      getItem: sinon.stub(),
+      removeItem: sinon.stub(),
+      setItem: sinon.stub()
     }
-    throw new Error('Test didn\'t throw')
+  })
+  it('should not affect localStorage if its data is more recent', () => {
+    localStorageOverride.getItem.returns(LARGER_SINCE)
+
+    loadDataIntoLocalStorage({ serializedData: { since: SMALLER_SINCE } }, localStorageOverride)
+
+    expect(localStorageOverride.getItem.calledOnce).to.equal(true)
+    expect(localStorageOverride.removeItem.called).to.equal(false)
+    expect(localStorageOverride.setItem.called).to.equal(false)
+  })
+
+  it('should clean up localStorage and change till to more recent value with larger since value', () => {
+    const removedItemKey = 'SPLITIO.should_be_cleared'
+    localStorageOverride.getItem.onFirstCall().returns(SMALLER_SINCE)
+    localStorageOverride[removedItemKey] = {}
+
+    const serializedData = { segmentsData: {}, since: LARGER_SINCE, splitsData: {}, usingSegmentsCount: 0 }
+    loadDataIntoLocalStorage({ serializedData }, localStorageOverride)
+
+    expect(localStorageOverride.removeItem.calledWith(removedItemKey)).to.equal(true)
+    expect(localStorageOverride.setItem.firstCall.calledWith('SPLITIO.splits.till', LARGER_SINCE)).to.equal(true)
+  })
+
+  it('should load serialized split data into localStorage properly', () => {
+    const serializedExperimentOne = 'serialized_experiment_1'
+    const serializedExperimentTwo = 'serialized_experiment_2'
+    const splitsData = {
+      experiment_1: serializedExperimentOne,
+      experiment_2: serializedExperimentTwo
+    }
+    localStorageOverride.getItem.onFirstCall().returns(SMALLER_SINCE)
+
+    const serializedData = { segmentsData: {}, since: LARGER_SINCE, splitsData, usingSegmentsCount: 0 }
+    loadDataIntoLocalStorage({ serializedData }, localStorageOverride)
+
+    expect(localStorageOverride.setItem.calledWith('SPLITIO.split.experiment_1', serializedExperimentOne)).to.equal(true)
+    expect(localStorageOverride.setItem.calledWith('SPLITIO.split.experiment_2', serializedExperimentTwo)).to.equal(true)
+  })
+
+  it('should load segments data into localStorage properly', () => {
+    const userId = 'visitor_guid_1'
+    const usingSegmentsCount = 2
+    const segmentsData = {
+      segment_1: [userId, 'visitor_guid_2'],
+      segment_2: [userId, 'visitor_guid_3']
+    }
+    localStorageOverride.getItem.onFirstCall().returns(SMALLER_SINCE)
+
+    const serializedData = { segmentsData, since: LARGER_SINCE, splitsData: {}, usingSegmentsCount }
+    loadDataIntoLocalStorage({ serializedData, userId }, localStorageOverride)
+
+    expect(localStorageOverride.setItem.calledWith('SPLITIO.splits.usingSegments', usingSegmentsCount)).to.equal(true)
+    expect(localStorageOverride.setItem.calledWith('visitor_guid_1.SPLITIO.segment.segment_1', '1')).to.equal(true)
+    expect(localStorageOverride.setItem.calledWith('visitor_guid_1.SPLITIO.segment.segment_2', '1')).to.equal(true)
   })
 })
